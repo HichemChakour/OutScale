cc6d0ec91e15f33a1e85fa4aa2c909f9901a7891
RobinKiefer2000 <robin.kiefer@alumni.univ-avignon.fr>
2025-03-10 18:29:50 +0100

    ta grand mere

--- Outscale/src/main.rs
+++ Outscale/src/main.rs
@@ -1,3 +1,14 @@
+mod items;
+
 fn main() {
-    println!("Hello, world!");
+    // Importer les classes
+    use items::equipment::Sword;
+
+    // Créer une instance de Sword
+    let sword = Sword::new("Épée de légende".to_string(), 10, 2.5);
+
+    // Appeler les méthodes de Sword
+    sword.display_info();
+    println!("Puissance : {}", sword.calculate_power());
+    println!("Le zizi de Amadou est vreuuuument énormimous bebou, puis-je le lécher goulûment ?")
 }
